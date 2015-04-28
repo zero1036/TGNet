47ab51e5d96a28123c91584f1f037d6311f6d0ac
zero1036 <t799162239@hotmail>
2015-04-28 18:48:27 +0800

    TW.Web-创建apibase控制器

--- TG.TW/TW.Web/Controllers/Base/ApiControllerBase.cs
+++ TG.TW/TW.Web/Controllers/Base/ApiControllerBase.cs
@@ -7,7 +7,10 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Security;
 using System.Web.Http;
+using System.Net;
+using System.Net.Http;
 using System.Web.Http.Controllers;
+using EG.WeChat.Utility.Tools;
 
 namespace TW.Web.Controllers
 {
@@ -18,7 +21,41 @@ namespace TW.Web.Controllers
     [BasicAuthentication]
     public abstract class ApiControllerBase : ApiController
     {
+        /// <summary>
+        /// 执行方法并捕获错误
+        /// </summary>
+        /// <param name="pInterface"></param>
+        /// <param name="action"></param>
+        protected HttpResponseMessage ExecuteTryCatch<VModel>(Func<VModel> action)
+        {
+            CActionResult pActionResult;
+            try
+            {
+                var vm = action.Invoke();
+                pActionResult = new CActionResult() { ok = true, message = "", data = vm };
+                return Request.CreateResponse(HttpStatusCode.OK, pActionResult);
+            }
+            catch (Exception ex)
+            {
+                //转换异常
+                var pEGResult = EGExceptionOperator.ConvertException(ex);
+                //转换Action结果
+                pActionResult = new CActionResult() { ok = false, message = pEGResult.Message, data = null };
+                //写入日志
+                if (pEGResult.IsLog)
+                    Logger.Log4Net.Info(string.Format("异常代码：{0};异常信息：{1}", pEGResult.ExCode, pEGResult.Message));
 
+                //消息不为空时，确保异常消息返回前端
+                if (pEGResult.StCode != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, pActionResult);
+                }
+                else
+                {
+                    return Request.CreateResponse((HttpStatusCode)pEGResult.StCode.Value, pActionResult);
+                }
+            }
+        }
     }
     /// <summary>
     /// UserAuthModel
