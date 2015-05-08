02cc9d219bcf095c1ddb7c1f739baf749eae3034
zero1036 <t799162239@hotmail.com>
2015-05-08 08:06:52 +0800

    TW.Web-添加通告app

--- TG.TW/TW.Web/app/scripts/controllers/newsModelCtrlWa.js
+++ TG.TW/TW.Web/app/scripts/controllers/newsModelCtrlWa.js
@@ -1,44 +1,43 @@
 ﻿/// <reference path="expCtrlWa.js" />
 'use strict';
-define(['controllers/controllers', 'services/commonService', 'services/matterService']
-        , function (controllers) {
-            controllers.controller('newsModelCtrlWa', ['$scope', '$location', '$routeParams', 'commonService', 'matterService', function ($scope, $location, $routeParams, commonService, matterService) {
-                //监听下拉pullDown事件
-                $scope.$on('pullDownAction', function (e, myScroll) {
-                    myScroll.refresh();
-                });                //监听上拉pullup事件                $scope.$on('pullUpAction', function (e, myScroll) {
-                });                //监听返回事件                $scope.$on('goBack', function (e, param) {
-                    if (commonService.returnPath != undefined && commonService.returnPath != null && commonService.returnPath != "")
-                        $location.path(commonService.returnPath);
-                });                //
-                $scope.childInit = function () {
-                    //$scope.$emit('childInit', false);
-                    $scope.loadCurNewsModel(null, false);
-                }
-                $scope.loadCurNewsModel = function (myScroll, bRefresh) {
-                    if (commonService.curNewsModel != undefined && commonService.curNewsModel != null) {
-                        $scope.curNewsModel = commonService.curNewsModel;
-                        angular.element("#nmw-div-content").html($scope.curNewsModel.content);
-                        commonService.curNewsModel = null;
+define(['controllers/controllers', 'services/baseUnionService', 'services/matterService'], function (controllers) {
+    controllers.controller('newsModelCtrlWa', ['$scope', '$location', '$routeParams', 'commonService', 'matterService', function ($scope, $location, $routeParams, commonService, matterService) {
+        //监听下拉pullDown事件
+        $scope.$on('pullDownAction', function (e, myScroll) {
+            myScroll.refresh();
+        });        //监听上拉pullup事件        $scope.$on('pullUpAction', function (e, myScroll) {
+        });        //监听返回事件        $scope.$on('goBack', function (e, param) {
+            if (commonService.returnPath != undefined && commonService.returnPath != null && commonService.returnPath != "")
+                $location.path(commonService.returnPath);
+        });        //
+        $scope.childInit = function () {
+            //$scope.$emit('childInit', false);
+            $scope.loadCurNewsModel(null, false);
+        };
+        $scope.loadCurNewsModel = function (myScroll, bRefresh) {
+            if (commonService.curNewsModel != undefined && commonService.curNewsModel != null) {
+                $scope.curNewsModel = commonService.curNewsModel;
+                angular.element("#nmw-div-content").html($scope.curNewsModel.content);
+                commonService.curNewsModel = null;
 
-                        if (bRefresh) {
-                            if (myScroll != undefined && myScroll != null) {
-                                myScroll.refresh();
-                            }
-                        }
-                        else {
-                            $scope.$emit('childInit', false);
-                        }
+                if (bRefresh) {
+                    if (myScroll != undefined && myScroll != null) {
+                        myScroll.refresh();
                     }
-                    else {
-                        if (commonService.returnPath != undefined && commonService.returnPath != null && commonService.returnPath != "")
-                            $location.path(commonService.returnPath);
-                    }
-
                 }
-
+                else {
+                    $scope.$emit('childInit', false);
+                }
             }
-            ]
-            )
-        }
+            else {
+                if (commonService.returnPath != undefined && commonService.returnPath != null && commonService.returnPath != "")
+                    $location.path(commonService.returnPath);
+            }
+
+        };
+
+    }
+    ]
+    )
+}
 );
---
+++ TG.TW/TW.Web/app/scripts/informMain.js
@@ -0,0 +1,142 @@
+﻿require.config({
+    paths: {
+        /*
+         * 引入类库
+         */
+        // jQuery
+        jquery: 'http://cdn.bootcss.com/jquery/1.9.0/jquery.min',
+        //// bootstrap
+        //bootstrap: 'vendor/bootstrap/js/bootstrap',
+        // Angular
+        angular: 'http://cdn.bootcss.com/angular.js/1.3.15/angular.min',
+        cookies: 'vendor/angular/angular-cookies.min',
+        route: 'vendor/angular/angular-route.min',
+        touch: 'vendor/angular/angular-touch.min',
+        // domReady
+        domReady: 'vendor/domReady',
+        //// xml to json
+        //x2js: 'vendor/x2js/xml2json',
+        //// angular-file-uploader
+        //uploader: 'vendor/angular-file-uploader/angular-file-upload',
+        // iscroll
+        iscroll: 'vendor/ace/iscroll.min'
+    },
+    shim: {
+        /*
+         *对引入的类库，表明依赖关系，并暴露名称
+         *需要把此处的名字，在app.js文件中define写上才能使用
+         */
+        angular: {
+            deps: ['jquery'],
+            exports: 'angular'
+        },
+        cookies: {
+            deps: ['angular']
+        },
+        route: {
+            deps: ['angular']
+        },
+        touch: {
+            deps: ['angular']
+        },
+        //bootstrap: {
+        //    deps: ['jquery']
+        //},
+        //x2js: {
+        //    deps: ['jquery']
+        //},
+        //uploader: {
+        //    deps: ['angular']
+        //}
+    }
+});
+require([
+	'angular',
+	'orgApp',
+	'domReady',
+    'iscroll',
+    // 自定义controllers,services,directives,filters都需要在这里添加路径
+    //父级控制
+    'controllers/waMainCtrl',
+    //素材
+    'controllers/newsModelCtrlWa',
+    //通告
+    'controllers/informCtrlWa',
+],
+function (angular, orgApp, domReady, iscroll) {
+    'use strict';
+    orgApp.constant('ACCESS_LEVELS', {
+        pub: 1,
+        user: 2
+    });
+
+    orgApp.config(['$routeProvider', '$httpProvider', 'ACCESS_LEVELS',
+        function ($routeProvider, $httpProvider, ACCESS_LEVELS) {
+            $routeProvider
+            .when('/home', {
+                templateUrl: 'views/home.html',
+                controller: 'waMainCtrl'
+            })
+            //通告--公司动态
+            .when('/informsPubWa', {
+                templateUrl: 'views/inform/informsPubWa.html',
+                controller: 'informCtrlWa',
+                access_level: ACCESS_LEVELS.pub
+            })
+            //通告--内部通告
+            .when('/informsPvtWa', {
+                templateUrl: 'views/inform/informsPvtWa.html',
+                controller: 'informPvtCtrlWa',
+                access_level: ACCESS_LEVELS.pub
+            })
+            //素材--段落详细内容
+            .when('/newsModelWa', {
+                templateUrl: 'views/matter/newsModelWa.html',
+                controller: 'newsModelCtrlWa',
+                access_level: ACCESS_LEVELS.pub
+            });// end
+
+
+            $httpProvider.defaults.withCredentials = true;
+            $httpProvider.defaults.useXDomain = true;
+            delete $httpProvider.defaults.headers.common['X-Requested-With'];
+
+            $httpProvider.interceptors.push(function ($q, $location, $rootScope) {
+                return {
+                    'response': function (resp) {
+                        return resp;
+                    },
+                    'responseError': function (rejection) {
+                        // 错误处理
+                        switch (rejection.status) {
+                            case 401:
+                                if (rejection.config.url !== 'api/login')
+                                    // 如果当前不是在登录页面
+                                    $rootScope.$broadcast('auth:loginRequired');
+                                break;
+                            case 403:
+                                $rootScope.$broadcast('auth:forbidden');
+                                break;
+                            case 404:
+                                $rootScope.$broadcast('page:notFound');
+                                break;
+                            case 500:
+                                $rootScope.$broadcast('server:error');
+                                break;
+                        }
+                        return $q.reject(rejection);
+                    }
+
+                };
+            });
+        }
+    ]);
+
+    domReady(function () {
+        angular.bootstrap(document, ['eOrderingApp']);
+
+        $('html').addClass('ng-app: eOrderingApp');
+
+    })
+});
+
