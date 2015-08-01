675b768a5d710e392e6ecdd08447777ba654927f
zero1036 <t799162239@hotmail.com>
2015-08-01 11:01:03 +0800

    lzc:xUnitTest加入moq实例

--- EG.Wechat/xUnitTest/Class1.cs
+++ EG.Wechat/xUnitTest/Class1.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+using Moq;
 using TW.Platform.Sys;
 using TW.Platform.Model;
 
@@ -16,11 +17,11 @@ namespace xUnitTest
             System.Console.WriteLine("Init");
         }
 
-        //[Fact]
-        //public void TestAdd()
-        //{
-        //    Assert.Equal<int>(5, 2 + 3);
-        //}
+        [Fact]
+        public void TestAdd()
+        {
+            Assert.Equal<int>(5, 2 + 3);
+        }
 
         [Fact(Timeout = 900)]//指定超时为900ms 
         public void TestTimeout()
@@ -30,9 +31,28 @@ namespace xUnitTest
         }
 
         [Fact]
-        public void TestAdd1()
+        public void TestMoqInterface()
         {
-            
+            var mo = new Mock<IFake>();
+
+            mo.Setup(p => p.DoSomething("Ping")).Returns(true);
+
+            Assert.Equal<bool>(true, mo.Object.DoSomething("Ping"));
+        }
+
+        [Fact]
+        public void TestMoqClass()
+        {
+            var mo = new Mock<FakeClass>();
+
+            mo.Setup(p => p.DoSomething("Ping")).Returns(true);
+
+            Assert.Equal<bool>(true, mo.Object.DoSomething("Ping"));
+        }
+
+        [Fact]
+        public void TestOrg1()
+        {
             var pUser = new CurUserM();
             pUser.SysUserId = 1;
             //设置当前用户
@@ -43,25 +63,22 @@ namespace xUnitTest
             Assert.NotEmpty(v);
         }
 
-        //[Fact]
-        //public void TestObjectX()
-        //{           
-
-        //    //var pUser = new CurUserM();
-        //    //pUser.SysUserId = 1;
-        //    ////设置当前用户
-        //    //SysCurUser.SetCurUser(pUser);
-
-        //    //TW.Platform.BL.OrgBL org = new TW.Platform.BL.OrgBL();
-        //    //var v = org.GetUsersByDepId(1);
-        //    //Assert.NotEmpty(v);
-
-        //    Assert.Equal<int>(5, 2 + 3);
-        //}
-
         public void Dispose()
         {
             System.Console.WriteLine("Dispose");
+        }
+    }
+
+    public interface IFake
+    {
+        bool DoSomething(string actionName);
+    }
+
+    public class FakeClass
+    {
+        public virtual bool DoSomething(string actionName)
+        {
+            return false;
         }
     }
 }
