5432538e66e6590b5a13e9cd6bec86885a157680
zero1036 <t799162239@hotmail.com>
2016-03-03 07:56:19 +0800

    添加单元测试

---
+++ TG.Example/TG.Base/Redis/RedisTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace TG.Example
+{
+    public class RedisTest
+    {
+        /// <summary>
+        /// 批量插入
+        /// </summary>
+        public void StringSet()
+        {
+            IDatabase redisdb = RedisProvider.redis.GetDatabase();
+
+            var dic = new Dictionary<RedisKey, RedisValue>();
+
+            for (var i = 1; i <= 100000; i++)
+            {
+                dic.Add("Num" + i, i);
+            }
+
+            redisdb.StringSet(dic.ToArray(), When.Always);
+        }
+    }
+}
