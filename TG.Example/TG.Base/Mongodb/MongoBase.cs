5098fa07c2841ac808d39978a15c78cb91834216
zero1036 <799162239@qq.com>
2016-05-19 06:48:05 +0800

    添加mongo测试

---
+++ TG.Example/TG.Base/Mongodb/MongoBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace TG.Example
+{
+    public class MongoBase
+    {
+    }
+}
---
+++ TG.Example/TG.Example.Test/Unit/MongoUnit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TG.Example;
+
+namespace TG.Example.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
