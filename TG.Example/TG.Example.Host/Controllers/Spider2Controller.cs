d879592f3257a755622e8e668c86058411bc1fd1
zero1036 <799162239@qq.com>
2016-06-30 06:56:57 +0800

    添加爬虫测试

---
+++ TG.Example/TG.Example.Host/Controllers/Spider2Controller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TG.Example.Host.Controllers
+{
+    public class Spider2Controller : Controller
+    {
+        //
+        // GET: /Spider2/
+        public ActionResult Index()
+        {
+            return Content("Index2");
+        }
+
+
+        public ActionResult Me()
+        {
+            return View();
+        }
+	}
+}
---
+++ TG.Example/TG.Example.Host/Controllers/SpiderController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TG.Example.Host.Controllers
+{
+    public class SpiderController : Controller
+    {
+        //
+        // GET: /Spider/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ContentResult Phone()
+        {
+            return Content("123");
+        }
+
+        public ContentResult Address()
+        {
+            return Content("Address");
+        }
+
+	}
+}
