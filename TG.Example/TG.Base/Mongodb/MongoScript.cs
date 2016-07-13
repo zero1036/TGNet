229971174d157a2ec5ae595dc18d1a6f0197a6fd
zero1036 <799162239@qq.com>
2016-07-13 21:24:16 +0800

    添加实例

---
+++ TG.Example/TG.Base/Mongodb/MongoScript.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Shared;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.Serializers;
+
+namespace TG.Example.Mongodb
+{
+    public class MongoScript
+    {
+        /// <summary>
+        /// 同时更新多个
+        /// </summary>
+        public void UpdateMany()
+        {
+            IMongoDatabase db = MongoBase.GetDatabase();
+            var col = db.GetCollection<Foo>("foo");
+
+            var doc = new BsonDocument { { "$set", new BsonDocument { { "Name", "droba" }, { "Age", 40 } } } };
+            UpdateResult res = col.UpdateMany(x => x.Sex == 1, doc);
+
+            System.Diagnostics.Debug.WriteLine(res.ToJson());
+        }
+    }
+}
