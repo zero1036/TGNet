14616b00d40c6b2869b93ec3d8167e9b309dfd23
linzc <linzicong@ppmoney.com>
2016-08-05 16:31:02 +0800

    test test

--- TG.Example/TG.Base/Graylog/GraylogApi.cs
+++ TG.Example/TG.Base/Graylog/GraylogApi.cs
@@ -11,18 +11,7 @@ namespace TG.Example
     {
         public void SearchKeyword()
         {
-            //string loginUrl = "http://192.168.1.86:12900/system/sessions";
-
-            //object target = new
-            //{
-            //    username = "dev",
-            //    password = "123456",
-            //    host = "192.168.100.122"
-            //};
-
-            //var loginResult = HttpHelper.HttpPost(loginUrl, 1000, null, JsonConvert.SerializeObject(target));
-
-            string domain = "http://192.168.1.86:12900";
+            string domain = "http://192.168.1.xx:xxxx";
             string url = string.Format("{0}/search/universal/keyword?query=source%3Aiis_wx_wap%20%26%26%20message%3A'ERROR'&keyword=2%20weeks%20ago%20to%20wednesday&fields=message", domain);
             string auth = "Basic ZGV2OjEyMzQ1Ng==";
 
